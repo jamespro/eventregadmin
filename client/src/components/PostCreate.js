1dcdccf44bf2a2956334f2de69d01550710ae9bb
James Prickitt <jwprickitt@gmail.com>
2021-06-26 10:58:32 -0400

    Add Edit

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Create, SimpleForm, TextInput, DateInput } from 'react-admin'
 
 const PostCreate = (props) => {
-    return <Create {...props}>
+    return <Create title='Create a post' {...props}>
         <SimpleForm>
             <TextInput source='title' />
             <TextInput multiline source='body' />
