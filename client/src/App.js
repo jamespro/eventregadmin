2049dac26e5ceb3daf476fe9affaffa5d566d444
James Prickitt <jwprickitt@gmail.com>
2021-06-27 23:39:17 -0400

    Converting to using API, setting up API

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin'
+// import restProvider from 'ra-data-simple-rest'
+import myDataProvider from './myDataProvider'
+import {fetchJson as httpClient} from './httpClient'
+import authProvider from './authProvider';
 import PostList from './components/PostList'
 import PostCreate from './components/PostCreate'
 import PostEdit from './components/PostEdit'
 import UserList from './components/UserList'
 import UserCreate from './components/UserCreate'
 import UserEdit from './components/UserEdit'
-require('dotenv').config({path:'./config/.env'})
+
+const DB_STRING = 'mongodb + srv://jamespro:eventregRc9jfmij!@cluster0.13huq.mongodb.net/devtest?retryWrites=true&w=majority'
+
+const dataProvider = myDataProvider(DB_STRING, httpClient);
 
 function App() {
     return (
-      <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Admin dataProvider={dataProvider} authProvider={authProvider} >
             <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
-            <Resource name='users' list={UserList} create={UserCreate} edit={UserEdit} />
+            <Resource name='users' list={UserList} create={UserCreate} show={ShowGuesser} edit={EditGuesser} />
       </Admin>
   );
 }
